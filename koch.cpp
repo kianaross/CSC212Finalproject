bd2ffb3c503d01a5e41d802ba0c49fb8356cdbf5
kianaross <71087588+kianaross@users.noreply.github.com>
2020-12-06 20:45:20 -0500

    Create koch.cpp

---
+++ koch.cpp
@@ -0,0 +1,73 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+//F : Move forward
+
+//- : Turn left by some degree
+
+//+: Turn right by some degree
+
+//starter code given in lab
+std::string koch_snowflake(int order);
+void snowflake(int order, std::string &commands);
+
+//main function
+int main(int argc, char *argv[]){
+    //save to outfile koch.txt
+    std::ofstream of("koch.txt");
+    //order as command line argument
+    int order = std::stoi(argv[1]);
+    //write to output, result from snowflake function
+    of << koch_snowflake(order);
+    /*of << flake(order);*/
+}
+
+//function to draw all sides of snowflake (3 sides)
+std::string koch_snowflake(int order){
+    //initialize string to empty
+    std::string commands = "";
+	// This handles generating the 3 'sides' of the snowflake.
+    for(int i = 0; i < 4; i++){
+        //call snowflake function with order and commands passed in
+        snowflake(order, commands);
+        commands += "+ + ";
+    }
+    //return the final string
+    return commands;
+}
+
+//snowflake function to draw one side of snowflake
+void snowflake(int order, std::string &commands){
+    //base case (order 0 draws triangle)
+    if(order == 0){
+        commands.append("F ");
+        return;
+    }
+    //recursion here
+    else{
+        //recursive call
+        snowflake(order - 1, commands);
+           //draw line
+        commands.append("F");
+        // add - (left turn)
+        commands.append("-");
+        //recursive call
+        snowflake(order-1, commands);
+        //draw line
+        commands.append("F");
+        // add + (right turn)
+        // add + (right turn
+        commands.append("+");
+        commands.append("+");
+        snowflake(order-1, commands);
+        //draw line
+        commands.append("F");
+        //add - (left turn)
+        commands.append("-");
+        snowflake(order-1, commands);
+        //draw line
+        commands.append("F");
+    }
+}
